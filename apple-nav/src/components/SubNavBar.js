d9fd69f1aa975093940e842dac4616be2a84896e
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-26 21:01:32 -0500

    Correct sandwiching

--- apple-nav/src/components/SubNavBar.js
+++ apple-nav/src/components/SubNavBar.js
@@ -3,23 +3,25 @@ import { Route, NavLink } from "react-router-dom"
 import "./SubNavBar.scss"
 
 const SubNavBar = ({ nav }) => (
-  <nav className="sub-nav">
+  <>
     {Object.entries(nav).map(([route, attrs]) => (
       <Route
         path={`/${route}`}
-        render={props =>
-          attrs.map(({ imgUrl, route: subroute, name }) => (
-            <NavLink to={`/${route}/${subroute}`} {...props}>
-              <div className="sub-nav-item">
-                <img src={imgUrl} />
-                <span>{name}</span>
-              </div>
-            </NavLink>
-          ))
-        }
+        render={props => (
+          <nav className="sub-nav">
+            {attrs.map(({ imgUrl, route: subroute, name }) => (
+              <NavLink to={`/${route}/${subroute}`} {...props}>
+                <div className="sub-nav-item">
+                  <img src={imgUrl} />
+                  <span>{name}</span>
+                </div>
+              </NavLink>
+            ))}
+          </nav>
+        )}
       />
     ))}
-  </nav>
+  </>
 )
 
 export default SubNavBar
